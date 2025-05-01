0dc82a7aaab1720e85a8e6de5673c9689531fea4
Robert Schwartz <robert70710@gmail.com>
2025-05-01 18:54:00 +0300

    Fix: Removed BOM from App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-﻿import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import {
